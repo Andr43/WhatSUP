22043577dafeb2ddc4e37fc1ed9237edc32168b3
Andrey Musinov <andrew.musinov@gmail.com>
2024-01-29 23:44:43 +0000

    Made AboutUs component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,12 @@
 import { React } from "react";
 import First from "./First/First.js";
+import Main from "./Main/Main.js";
 
 function App() {
   return (
     <>
       <First/>
+      <Main/>
     </>
   );
 }
---
+++ frontend/src/components/Main/Main.js
@@ -0,0 +1,10 @@
+import { React } from "react";
+import AboutUs from "./AboutUs/AboutUs.js";
+
+function Main() {
+  return (
+<AboutUs />
+  );
+}
+
+export default Main;
