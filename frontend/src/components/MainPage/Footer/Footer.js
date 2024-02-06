8d94004527e87da7b4e4ced835898d71a20ec3e5
Andrey Musinov <andrew.musinov@gmail.com>
2024-02-06 13:10:25 +0000

    Made Footer adaptive

--- frontend/src/components/MainPage/Footer/Footer.js
+++ frontend/src/components/MainPage/Footer/Footer.js
@@ -6,8 +6,12 @@ function Footer() {
     <footer className="footer main__section">
       <div className="footer__contacts">
         <h4 className="footer__logo">What?SUP!</h4>
-        <p className="footer__par footer__par_phone">+351237128097</p>
-        <p className="footer__par footer__par_email">info@whatsup.pt</p>
+        <Link className="footer__par footer__par_phone" to="+351237128097">
+        +351237128097
+        </Link>
+        <Link className="footer__par footer__par_email" to="">
+        info@whatsup.pt
+        </Link>
         <p className="footer__copyright">
           © 2024 What?SUP!, Inc. Terms & Privacy
         </p>
